e9fa0a9d7ead00dd1ebc90c75df835564595b032
Ashok Kumar Shrestha <59673063+ashokstha61@users.noreply.github.com>
2024-09-23 06:31:39 +0545

    authentication on signin and signup

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,17 +1,33 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import { Link } from "expo-router";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
+import { signIn } from "../../lib/appwrite";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: " ",
     password: " "
   });
-  const Submit = () => {};
+
+  const Submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "Please fill all the necessary field.");
+    }
+    setIsSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   const [isSubmitting, setIsSubmitting] = useState(false);
   return (
     <SafeAreaView className="bg-primary h-full">
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,18 +1,32 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
   const [form, setForm] = useState({
-    username: " ",
-    email: " ",
-    password: " "
+    username: "",
+    email: "",
+    password: ""
   });
-  const Submit = () => {};
+  const Submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert("Error", "Please fill all the necessary field.");
+    }
+    setIsSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   const [isSubmitting, setIsSubmitting] = useState(false);
   return (
     <SafeAreaView className="bg-primary h-full">
